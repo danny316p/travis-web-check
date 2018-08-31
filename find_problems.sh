342ee30779153e5abbba0be4fcf7f45a63ec697a
danny316p <316danny@gmail.com>
2018-08-30 22:54:50 -0400

    Try to clean up a bit and make things more readable

--- find_problems.sh
+++ find_problems.sh
@@ -3,12 +3,22 @@
 
 function convert_all_to_markdown() {
     FILE_FORMAT="$@";
-    for FILENAME in `ls *.$FILE_FORMAT`; do
-        echo "$FILENAME";
-        MARKDOWN_VERSION=`sed "s/\.$FILE_FORMAT$/.md/" <<< "$FILENAME"`;
-        echo "$MARKDOWN_VERSION";
-        lynx --dump "$FILENAME" > "$MARKDOWN_VERSION";
-    done;
+    if [ -f = "*.$FILE_FORMAT" ]
+        then
+            for FILENAME in `ls *.$FILE_FORMAT`; do
+                echo "$FILENAME";
+                MARKDOWN_VERSION=`sed "s/\.$FILE_FORMAT$/.md/" <<< "$FILENAME"`;
+                echo "$MARKDOWN_VERSION";
+                lynx --dump "$FILENAME" > "$MARKDOWN_VERSION";
+            done;
+    fi
+}
+
+function wrap_command() {
+    COMMAND="$@";
+    echo "Running $COMMAND";
+    `"$COMMAND"`;
+    echo "Completed $COMMAND";
 }
 
 convert_all_to_markdown "htm";
@@ -16,6 +26,6 @@ convert_all_to_markdown "html";
 convert_all_to_markdown "xml";
 
 # Now that we've converted everything to text/markdown, we can run text/markdown tools over the results
-proselint *.md
-cat *.md | aspell --list
+wrap_command "proselint *.md";
+wrap_command "cat *.md | aspell --list";
 
